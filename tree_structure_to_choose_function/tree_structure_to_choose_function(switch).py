50dbcf53c83f086c6adfb6eaba628c7d22e0eda7
liria <36659752+valenteers@users.noreply.github.com>
2022-05-10 02:25:18 +0700

    test switch_case
    Not good enough becoz canot keep condition in variable(set) like if_else

---
+++ tree_structure_to_choose_function/tree_structure_to_choose_function(switch).py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3  
+
+from email.policy import default
+
+
+depthNode = 0
+deepNode = {'1','2-1','node2-1','go to deepnode'}
+
+if __name__ == "__main__":
+    while depthNode == 0:
+        print ("Welcome to node 0 this is begin")
+        node = input("Choose node\n")
+    #------------------------------------ node 1 ----------------------------------------#
+        match node:
+            case '1'|'node1':
+                depthNode = depthNode + 1
+                while depthNode == 1:
+                    print ("Welcome to node 1")
+                    node = input("Choose node\n")
+        #------------------------------------ node 1-1 ----------------------------------------#
+                    match node:           
+                        case '1'|'1-1'|'node1-1'|'restart':
+                            print ('Welcome to node 1-1')
+                            depthNode = 0
+                            print ('Now you will back to begin')
+        #------------------------------------ node 1-2 ----------------------------------------#
+                        case '2'|'1-2'|'node1-2'|'back':
+                            print ('Welcome to node 1-2')
+                            depthNode = depthNode-1
+                            print ('Now you will back to one step before')
+                        case default:
+                            print ('Plz choose node i have')
+                            depthNode = depthNode-1
+
+        #------------------------------------ node 2 ----------------------------------------#
+
+            case '2'|'node2':
+                depthNode = depthNode + 1
+                while depthNode == 1:
+                    print ("Welcome to node 2")
+                    node = input("Choose node\n")
+        #------------------------------------ node 2-1 ----------------------------------------#
+                    if node in deepNode:
+                        depthNode = depthNode + 1
+                        while depthNode == 2:
+                            print ("Welcome to node 2-1")
+                            node = input("Choose node\n")
+        #------------------------------------ node 2-1-1 ----------------------------------------#
+                            if node in {'1','2-1-1','node2-1-1'}:
+                                print ('Welcome to node 2-1-1')
+                                depthNode = int(input("Choose depth_node you need\n"))
+                                print ('Now you will back to that node')
+        #------------------------------------ node 2-1-2 ----------------------------------------#
+                            elif node in {'2','2-1-2','node2-1-2'}:
+                                print ('Welcome to node 2-1-2')
+                                print ('Now exit')
+                                exit() #Can instead of function you need to do
+                            else:
+                                print ('Plz choose node i have')
+                    else:
+                        print ('Plz choose node i have')
+                        depthNode = depthNode-1
+        #------------------------------------ no node ----------------------------------------#
+            case default:
+                print ('Plz choose node i have')
